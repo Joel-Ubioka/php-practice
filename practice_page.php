a88471f8df541c903e1088f62335ea64313a1586
morrhtechsolutions <joelubioka@gmail.com>
2022-04-13 14:36:37 -0700

    image upload

--- practice_page.php
+++ practice_page.php
@@ -10,15 +10,45 @@
 
 <body>
     <?php
+if(isset($_POST['upload']))
+{
+    $file = $_FILES['profile_picture'];
+    $file_name = $_FILES['profile_picture'] ['name'];
+    $file_tmp_name = $_FILES['profile_picture'] ['tmp_name'];
+    $file_size = $_FILES['profile_picture'] ['size'];
+    $file_error = $_FILES['profile_picture'] ['error'];
+    $file_type = $_FILES['profile_picture'] ['type'];
 
-$file = $_FILES['profile_picture'];
-$file_name = $_FILES['profile_picture'] ['name'];
-$file_tmp_name = $_FILES['profile_picture'] ['tmp_name'];
-$file_size = $_FILES['profile_picture'] ['size'];
-$file_error = $_FILES['profile_picture'] ['error'];
-$file_type = $_FILES['profile_picture'] ['type'];
+    $exploded_file = explode('.',$file_name);
+    $file_ext = strtolower(end($exploded_file));
+    $allowed_ext = array('jpg', 'jpeg', 'png','gif');
 
-$exploded_file = explode('.',$file_name);
+    if(in_array( $file_ext, $allowed_ext ))
+    {
+        if( $file_size < 1048576)
+        {
+           $file_name_new = uniqid('', true).".". $file_ext;
+           $file_destination = 'images/'.$file_name_new;
+           if(move_uploaded_file($file_tmp_name , $file_destination)) 
+           {
+            echo "<p style='color:green;'>succesfully uploaded</p>";
+            
+           }
+           else
+           {
+            echo  "<p style='color:red;'>Failed to upload</p>"; 
+           }
+        }
+        else
+        {
+            echo "<p style='color:red;'>You can not allowed to upload file more than 1mb</p>"; 
+        }
+    }
+    else
+    {
+        echo "<p style='color:red;'>You can not upload a file of this type</p>"; 
+    }
+}
 ?>
 
 
